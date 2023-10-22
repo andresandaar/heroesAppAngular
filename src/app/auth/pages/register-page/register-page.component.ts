66bee2d1eea814a8565f24b3cd90472739779849
unknown <davilaarroyo07@gmail.com>
2023-10-21 22:46:32 -0500

    primera version

---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-register-page',
+  templateUrl: './register-page.component.html',
+  /* styleUrls: ['./register-page.component.scss'] */
+})
+export class RegisterPageComponent {
+
+}
